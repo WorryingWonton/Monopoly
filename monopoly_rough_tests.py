d6ecd21d542f5e35bf3ae7fb22e9bd03cef983a2
WorryingWonton <ahcampbellster@gmail.com>
2018-10-25 15:11:49 -0700

    Cleanup of files no longer relevant to project

--- monopoly_rough_tests.py
+++
@@ -1,9 +0,0 @@
-from community_chest_functions import *
-from monopoly import *
-player_1 = Player('Leslie')
-card = Card('Pay Income Tax', receive_25_consultancy_fee)
-card.action(player_1)
-print(player_1.liquid_holdings)
-
-my_game = Monopoly()
-print(my_game.board)
