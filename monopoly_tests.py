489d16c8d3254dd73a37caa234b210be753534de
Alan Campbell <ahcampbellster@gmail.com>
2019-02-12 19:02:08 -0700

    Cleaned up unittests which no longer reflected how my code works
    Added tests to check Player cycling and removal behavior

--- monopoly_tests.py
+++ monopoly_tests.py
@@ -2,24 +2,150 @@ import unittest
 import monopoly
 import tiles
 import cards
+import monopoly_cl_interface
 
-def get_tile_positions(game, target=None, name=None):
+class HelperFunctions:
+
+    @staticmethod
+    def get_tile_positions(game, target=None, name=None):
+        """
+        Global helper function to find Tiles, either by Property.name or by Class reference.
+        :param game:
+        :param target:
+        :param name:
+        :return:
+        """
+        if target and name:
+            raise Exception('Cannot search for both an instance of a Tile and the name of the Property on a Tile, pass one or the other.')
+        if target:
+            return [tile.position for tile in game.board if isinstance(tile, target)]
+        if name:
+            ownable_tiles = list(filter(lambda tile: isinstance(tile, tiles.OwnableTile), game.board))
+            return [tile.position for tile in ownable_tiles if tile.property.name == name][0]
+
+
+class TestPlayerRemovalCases(unittest.TestCase):
+
     """
-    Global helper function to find Tiles, either by Property.name or by Class reference.
-    :param game:
-    :param target:
-    :param name:
-    :return:
+    These tests cover a variety of cases for removing Players from the game.
+    Each test covers a specific removal case (like removing the active Player, or removing the Player immediately ahead of the Active Player)
+    The goal of each test is to verify that if a Player is removed by whatever scenario the test covers, that the correct next Active Player will always be chosen.
+    The methods looked at in these tests are:
+        -Monopoly.remove_player()
+        -Monopoly.set_active_player()
+            -And by extension:  Monopoly.generate_in_game_players()
     """
-    if target and name:
-        raise Exception('Cannot search for both an instance of a Tile and the name of the Property on a Tile, pass one or the other.')
-    if target:
-        return [tile.position for tile in game.board if isinstance(tile, target)]
-    if name:
-        ownable_tiles = list(filter(lambda tile: isinstance(tile, tiles.OwnableTile), game.board))
-        return [tile.position for tile in ownable_tiles if tile.property.name == name][0]
+
+    def test_next_player_removal(self):
+        my_game = monopoly.Monopoly()
+        my_game.add_player('David')
+        my_game.add_player('Sara')
+        my_game.add_player('Alan')
+        my_game.add_player('Mark')
+        my_game.set_active_player()
+        self.assertEqual('David', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Sara', my_game.active_player.name)
+        my_game.remove_player(my_game.all_players[2])
+        my_game.set_active_player()
+        self.assertEqual(3, len(my_game.players))
+        self.assertEqual('Mark', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('David', my_game.active_player.name)
+
+    def test_active_player_removal(self):
+        my_game = monopoly.Monopoly()
+        my_game.add_player('David')
+        my_game.add_player('Sara')
+        my_game.add_player('Alan')
+        my_game.add_player('Mark')
+        my_game.set_active_player()
+        self.assertEqual('David', my_game.active_player.name)
+        my_game.remove_player(my_game.all_players[0])
+        self.assertEqual(4, len(my_game.players))
+        my_game.set_active_player()
+        self.assertEqual(3, len(my_game.players))
+        self.assertEqual('Sara', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Alan', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Mark', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Sara', my_game.active_player.name)
+
+    def test_player_removal_at_prior_index(self):
+        my_game = monopoly.Monopoly()
+        my_game.add_player('David')
+        my_game.add_player('Sara')
+        my_game.add_player('Alan')
+        my_game.add_player('Mark')
+        my_game.set_active_player()
+        my_game.set_active_player()
+        my_game.remove_player(my_game.all_players[0])
+        #The next active player should be Alan if David is removed, and the current active player is Sara
+        my_game.set_active_player()
+        self.assertEqual('Alan', my_game.active_player.name)
+
+    def test_player_removal_at_arbitrary_forward_index(self):
+        my_game = monopoly.Monopoly()
+        my_game.add_player('David')
+        my_game.add_player('Sara')
+        my_game.add_player('Alan')
+        my_game.add_player('Mark')
+        my_game.add_player('Bill')
+        my_game.add_player('Bob')
+        my_game.set_active_player()
+        my_game.remove_player(my_game.players[3])
+        self.assertEqual('David', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Sara', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Alan', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Bill', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Bob', my_game.active_player.name)
+        my_game.set_active_player()
+
+    def test_player_removal_at_arbitrary_prior_index(self):
+        my_game = monopoly.Monopoly()
+        my_game.add_player('David')
+        my_game.add_player('Sara')
+        my_game.add_player('Alan')
+        my_game.add_player('Mark')
+        my_game.add_player('Bill')
+        my_game.add_player('Bob')
+        my_game.set_active_player()
+        my_game.set_active_player()
+        my_game.set_active_player()
+        self.assertEqual('Alan', my_game.active_player.name)
+        my_game.remove_player(my_game.all_players[0])
+        my_game.set_active_player()
+        self.assertEqual('Mark', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Bill', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Bob', my_game.active_player.name)
+        my_game.set_active_player()
+        self.assertEqual('Sara', my_game.active_player.name)
+
+    def test_player_removal_at_last_index_in_players(self):
+        my_game = monopoly.Monopoly()
+        my_game.add_player('David')
+        my_game.add_player('Sara')
+        my_game.add_player('Alan')
+        my_game.add_player('Mark')
+        my_game.set_active_player()
+        my_game.remove_player(my_game.all_players[-1])
+        my_game.set_active_player()
+        self.assertEqual('Sara', my_game.active_player.name)
+        my_game.remove_player(my_game.all_players[2])
+        my_game.set_active_player()
+        self.assertEqual('David', my_game.active_player.name)
+
 
 class TestMonopolyInitialization(unittest.TestCase):
+
     def test_game_initialization(self):
         game_instance = monopoly.Monopoly()
         self.assertEqual(40, len(game_instance.board))
@@ -29,91 +155,11 @@ class TestMonopolyInitialization(unittest.TestCase):
         self.assertEqual(16, len(game_instance.community_deck.cards))
         self.assertEqual(0, game_instance.turns)
 
-class TestPlayerInitialization(unittest.TestCase):
-    def test_add_player_method(self):
-        game = monopoly.Monopoly()
-        game.add_player('Dave')
-        self.assertEqual('Dave', game.players[0].name)
-
-    def test_active_player_cycling(self):
-        game = monopoly.Monopoly()
-        game.add_player('Sara')
-        game.add_player('Bill')
-        self.assertEqual('Sara', game.active_player.name)
-        self.assertEqual('Bill', game.active_player.name)
-        self.assertEqual('Sara', game.active_player.name)
-
-class TestCardFunctions(unittest.TestCase):
-    """
-    Each card has a unique function associated with it.  In total, between the Chance and Community Chest decks, there
-    are 32 cards.  All Card functions read state, modify state, and return None.  Cards that modify a Player's position
-    need to be able to call game.board[game.active_player.position].perform_auto_actions(game=game), so that appropriate
-    actions are taken in accordance with the rules for the Tile said Player has been moved to.  This defines the
-    signature generically of each card function as:
-        def card_func(Monopoly):
-            return None
-    """
-    game_instance = monopoly.Monopoly()
-    game_instance.add_player('Bill')
-    game_instance.add_player('Sara')
-    game_instance.add_player('Mike')
-    game_instance.generate_in_game_players()
-    game_instance.set_active_player()
-    game_instance.active_player.liquid_holdings = 10000
-
-    def card_finder(self, card_name, deck):
-        return [card for card in deck.cards if card.name.lower() == card_name.lower()][0]
-
-    def test_advance_to_go(self, game=game_instance):
-        """
-        This Card appears in both the Chance and Community Chest decks.
-        :param Monopoly game:
-        :return:
-        """
-        #Move the active player to an arbitrary position on the Board, the Card should move the Active Player back to
-        #position 0.
-        game.active_player.position = 23
-        game.active_player.dealt_card = self.card_finder(card_name='Advance to Go', deck=game.chance_deck)
-        game.active_player.dealt_card.consume_card(game=game)
-        self.assertEqual(10200, game.active_player.liquid_holdings)
-        self.assertEqual(0, game.active_player.position)
-
-    def test_advance_to_illinois_ave(self, game=game_instance):
-        game.active_player.dealt_card = self.card_finder(card_name='Advance to Illinois Ave.', deck=game.chance_deck)
-        game.active_player.dealt_card.consume_card(game=game)
-        #Case 1, advancing to Illinois Avenue if the Tile is not owned
-        self.assertEqual(get_tile_positions(game=game, name='Illinois Avenue'), game.active_player.position)
-        self.assertEqual(10000, game.active_player.liquid_holdings)
-        #Case 2, another player owns the Tile
-        game.active_player.position = 0
-        game.players[1].property_holdings.append(game.board[get_tile_positions(game=game, name='Illinois Avenue')])
-        game.active_player.dealt_card = self.card_finder(card_name='Advance to Illinois Ave.', deck=game.chance_deck)
-        game.active_player.dealt_card.consume_card(game=game)
-        self.assertEqual(9980, game.active_player.liquid_holdings)
-
-    def test_cards_that_move_to_ownable_tiles(self, game=game_instance):
-        card_names = []
-
-
-
-
-class TestBankruptcyHandling(unittest.TestCase):
-    def test_bankruptcy_from_rent_assessed(self):
-        """
-        Player to Player Bankruptcy
-
-        """
-        pass
-
-    def test_bankruptcy_for_fees_assessed(self):
-        """
-        Player to Bank Bankruptcy
-        :return:
-        """
-        pass
+    def test_default_interface(self):
+        game_instance = monopoly.Monopoly()
+        self.assertEqual(True, isinstance(game_instance.interface, monopoly_cl_interface.CLInterface))
 
 class TestColorTile(unittest.TestCase):
-
 
     def test_is_color_group_filled(self):
         """
@@ -172,12 +218,6 @@ class TestColorTile(unittest.TestCase):
         game_instance.players[0].position = 39
         self.assertEqual(1325, game_instance.players[0].liquid_holdings)
         self.assertEqual(1675, game_instance.players[1].liquid_holdings)
-
-
-
-
-
-
 
 class TestIncomeTaxTile(unittest.TestCase):
     pass
